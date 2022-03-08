a18dba10cd8413c2984f583954c00288eb103f6d
Dan Tinsley <detinsley1s@gmail.com>
2022-03-08 03:30:24 -0600

    grid boxes change color when mouse hovers

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const board = document.querySelector('#board');
 for (let i = 0; i < 16*16; ++i) {
     let div = document.createElement('div');
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    });
     board.appendChild(div);
 }
