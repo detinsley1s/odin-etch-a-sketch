f367386e5a0897867e20177c9975d0951d4c110d
Dan Tinsley <detinsley1s@gmail.com>
2022-03-09 02:29:41 -0600

    grid boxes only change color when left mouse button is held down

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const board = document.querySelector('#board');
 for (let i = 0; i < 16*16; ++i) {
     let div = document.createElement('div');
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'black';
+    div.addEventListener('mouseover', (e) => {
+        if (e.buttons == 1) {
+            div.style.backgroundColor = 'black';
+        }
     });
     board.appendChild(div);
 }
