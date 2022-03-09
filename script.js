c2cb6aa4f933072b30d2d5c1d412d3dfaf671ccf
Dan Tinsley <detinsley1s@gmail.com>
2022-03-09 03:06:55 -0600

    refactor grid to a function to create any sized grid

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const board = document.querySelector('#board');
-for (let i = 0; i < 16*16; ++i) {
-    let div = document.createElement('div');
-    div.addEventListener('mouseover', (e) => {
-        if (e.buttons == 1) {
-            div.style.backgroundColor = 'black';
-        }
-    });
-    board.appendChild(div);
+
+function createGrid(size) {
+    while (board.hasChildNodes()) {
+        board.removeChild(board.firstChild);
+    }
+
+    for (let i = 0; i < size*size; ++i) {
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', (e) => {
+            if (e.buttons == 1) {
+                div.style.backgroundColor = 'black';
+            }
+        });
+        board.appendChild(div);
+    }
 }
+
+createGrid(16);
